0d66bda495c3c1ad02a2ef859e99bae4b2d413d4
blackorbit1 <enzo.dutra@gmail.com>
2019-04-17 09:55:13 +0200

    Ajout de la classe LiseurPixel

---
+++ licence3.image/codebar/src/main/java/org/codebar/LiseurPixels.java
@@ -0,0 +1,142 @@
+package org.codebar;
+
+import net.imagej.ImgPlus;
+import net.imglib2.RandomAccess;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.type.numeric.integer.IntType;
+
+import org.scijava.ItemIO;
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+import net.imagej.ImgPlus;
+import net.imglib2.RandomAccess;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.integer.IntType;
+
+public class LiseurPixels {
+	
+	
+	public static <T> String getCodeBinaire(ImgPlus<T> inputImage) {
+		long[] dims = new long[inputImage.numDimensions()];
+		inputImage.dimensions(dims);
+		RandomAccess<T> inputImageCursor = inputImage.randomAccess();
+
+		long[] posInputImage = new long[inputImage.numDimensions()];
+		posInputImage[1] = inputImage.dimension(1) / 2;
+		
+		int [] tab_bits = new int[95];
+		int longueur_bit = (int) (inputImage.dimension(0) / 95);
+		int longueur_changement = longueur_bit / 3;
+		
+		boolean debut = true;
+		boolean dernier_noir = false;
+		int nb_bit = 0;
+		int nb_pixels = 0;
+		int decompte_parcours_bordure = 0;
+		int nb_pixels_last_change = 0;
+
+		for (int i = 0; i < inputImage.dimension(0); i++) {
+			nb_pixels++;
+			posInputImage[0] = i;
+			inputImageCursor.setPosition(posInputImage);
+			boolean changement = false;
+			decompte_parcours_bordure--;
+			if(i > 1 && i < (inputImage.dimension(0) - 1)) {
+				
+				
+				posInputImage[0] = i;
+				inputImageCursor.setPosition(posInputImage);
+				int sum_gauche = (int) inputImageCursor.get().getRealDouble();
+				
+				posInputImage[0] = i+1;
+				inputImageCursor.setPosition(posInputImage);
+				int sum_droite = (int) inputImageCursor.get().getRealDouble();
+				
+				/*
+				 * Pourrait servir si y a des nuances mais ce n'est pas le cas
+				 * 
+				int sum_gauche = 0;
+				for(int i_gauche = i; i_gauche < i-longueur_changement; i_gauche++) {
+					posInputImage[0] = i_gauche;
+					inputImageCursor.setPosition(posInputImage);
+					sum_gauche += (int) inputImageCursor.get().getRealDouble();
+				}
+				
+				int sum_droite = 0;
+				for(int i_droite = i; i_droite < i+longueur_changement; i_droite++) {
+					posInputImage[0] = i_droite;
+					inputImageCursor.setPosition(posInputImage);
+					sum_droite += (int) inputImageCursor.get().getRealDouble();
+				}
+				*/
+				
+				// Si il detecte une bordure
+				if(Math.abs(sum_gauche - sum_droite) > 128 && decompte_parcours_bordure < 0) {
+					decompte_parcours_bordure = longueur_changement;
+					changement = true;
+				}
+			}
+			if(((double) inputImageCursor.get().getRealDouble()) > 128) { // TODO Dafuq le fuck ?
+				// Si c'est blanc
+				if(debut) {
+					// pass
+				} else if(nb_bit >= 3) {
+					if(dernier_noir) {
+						nb_bit++;
+						longueur_bit = (longueur_bit + nb_pixels)/2;
+						tab_bits[nb_bit] = 0;
+					} else {
+						// pass
+					}
+				} else if(!dernier_noir && (nb_pixels_last_change % longueur_bit) == 0) {
+					nb_bit++;
+					tab_bits[nb_bit] = 0;
+				} else if(dernier_noir) {
+					nb_bit++;
+					tab_bits[nb_bit] = 0;
+				} else if(!dernier_noir) {
+					nb_pixels_last_change++;
+				}
+				dernier_noir = false;
+				
+			} else {
+				// Si c'est noir
+				if(debut) {
+					debut = false;
+					tab_bits[nb_bit] = 1;
+					nb_bit++;
+				} else if (nb_bit >= 3) {
+					if(dernier_noir) {
+						// pass
+					} else {
+						nb_bit++;
+						longueur_bit = (longueur_bit + nb_pixels)/2;
+						tab_bits[nb_bit] = 1;
+					}
+				} else if(dernier_noir && (nb_pixels_last_change % longueur_bit) == 0) {
+					nb_bit++;
+					tab_bits[nb_bit] = 1;
+				} else if(!dernier_noir) {
+					nb_bit++;
+					tab_bits[nb_bit] = 1;
+				} else if(dernier_noir) {
+					nb_pixels_last_change++;
+				}
+				dernier_noir = true;
+			}
+		}
+		
+		String resultat = "";
+		for(int i = 0; i<tab_bits.length; i++) {
+			resultat += "" + tab_bits[i];
+		}
+		
+		System.out.println(resultat);
+		
+		return resultat;
+		
+	}
+}
